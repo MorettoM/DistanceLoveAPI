74663518f95debbc04e76490075df0da52b37f5d
Marcos Moretto <moretto@MacBook-Pro-de-Marcos.local>
2021-09-09 00:19:08 -0300

    test

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var socket = require('socket.io')
 var server = app.listen(4000, function(){
     console.log('listening for requests on port 4000,');
 });
-
 let io = socket(server)
 io.on('connection', function(socket){
   console.log(`${socket.id} is connected`);
