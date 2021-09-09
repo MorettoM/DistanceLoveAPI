42652be2132bdba46f370db5b7fca573c904ce51
Marcos Moretto <moretto@MacBook-Pro-de-Marcos.local>
2021-09-09 00:09:52 -0300

    test

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ var express = require('express');
 
 var app = express();
 
-const server = app.listen(8000, () => console.log("server running on port:" + 8000));
-const io = require("socket.io")();
+const server = require("http").createServer(app);
+const io = require("socket.io")()
 
 io.listen(server);
 
 io.on("connection", socket => {
-  console.log('user connected')
   socket.on("touch heart", msg => {
     console.log(msg);
   });
 });
+
+server.listen(8000, () => console.log("server running on port:" + 8000));
 
 module.exports = app;
