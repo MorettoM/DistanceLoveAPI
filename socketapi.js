44f06b1ed5d946d045a7410bda08a0ca7fc26e4f
Marcos Moretto <moretto@MacBook-Pro-de-Marcos.local>
2021-09-09 00:27:18 -0300

    test

--- socketapi.js
+++ socketapi.js
@@ -5,7 +5,24 @@ const socketapi = {
 
 // Add your socket.io logic here!
 io.on( "connection", function( socket ) {
-    console.log( "A user connected" );
+    socket.on("heart touch", msg => {
+        switch (msg) {
+            case 'marcos touched':
+                io.emit("marcos", msg);
+                break;
+            case 'marcos untouched':
+                io.emit("untouched", msg);
+                break;
+            case 'nacho touched':
+                io.emit("touched", msg);
+                break;
+            case 'nacho untouched':
+                io.emit("untouched", msg);
+                break;
+           default:
+                break;
+        }
+   });
 });
 // end of socket.io logic
 
